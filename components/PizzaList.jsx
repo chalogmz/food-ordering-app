183c69cbf8b627ac282c3a8a405c7b41788f3365
Salome Gomez <sgomez713@misena.edu.co>
2022-03-31 21:32:17 -0500

    se creo la lista de productos y la card de producto

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -1,8 +1,25 @@
-
+import styles from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard'
 
 const PizzaList = () => {
     return (
-        <div>PizzaList</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+            <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+
+            <div className={styles.wrapper}>
+                <PizzaCard/>
+                <PizzaCard/>
+                <PizzaCard/>
+                <PizzaCard/>
+                <PizzaCard/>
+                <PizzaCard/>
+                <PizzaCard/>
+                <PizzaCard/>
+            </div>
+        </div>
     )
 }
 
